e50e346341e25e54cc38e5b99166c0dd6b2d07ef
FernandoYZ <fernandozavala266@gmail.com>
2024-09-06 07:29:41 -0500

    feat(layout): Integrar Header y Footer en el layout principal
    - Se actualizó el archivo layout.tsx para incluir los componentes Header y Footer.
    - Se importaron los componentes desde las secciones correspondientes.
    - Se añadió un ícono de favicon en la metadata para mejorar la presentación del sitio.
    - El layout ahora renderiza el contenido de los hijos entre el Header y el Footer.
    
    No hay issues
    #Close 0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/sections/header";
+import Footer from "@/sections/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,6 +11,9 @@ export const metadata = {
   description: "Panel administrativo de Terranova Inmobiliaria SAC.",
   robots: "noindex, nofollow",
   viewport: "width=device-width, initial-scale=1, shrink-to-fit=no",
+  icons: {
+    icon: '/favicon.ico'
+  }
 };
 
 export default function RootLayout({
@@ -18,7 +23,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+        <Header/>
+        <Footer/>
+      </body>
     </html>
   );
 }
