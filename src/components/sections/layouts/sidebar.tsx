f31cdfaec36ec342695ad511979c8623e3d33513
FernandoYZ <fernandozavala266@gmail.com>
2024-09-07 23:14:47 -0500

    refactor(sidebar): Actualizar componente Sidebar para incluir Mobile y Desktop
    - Se actualizó el componente Sidebar para integrar MobileSidebar y DesktopSidebar, permitiendo una mejor adaptación a diferentes tamaños de pantalla.
    - Se definieron módulos de menú con títulos y elementos para ser utilizados en ambos componentes de barra lateral.
    
    No hay issues
    #Close 0

--- src/components/sections/layouts/sidebar.tsx
+++ src/components/sections/layouts/sidebar.tsx
@@ -1,8 +1,41 @@
-'use client';
+import MobileSidebar from "./Mobile/MobileSidebar";
+import DesktopSidebar from "./Desktop/DesktopSidebar";
 
-export default function Sidebar() {
-  return (
-    <div>Sidebar</div>
-  )
+interface Module {
+  title: string;
+  items: string[];
 }
 
+const modules: Module[] = [
+  {
+    title: "Módulo 1",
+    items: ["SubItem 1", "SubItem 2", "SubItem 3"],
+  },
+  {
+    title: "Módulo 2",
+    items: ["SubItem A", "SubItem B", "SubItem C"],
+  },
+  {
+    title: "Módulo 3",
+    items: ["SubItem X", "SubItem Y", "SubItem Z"],
+  },
+  {
+    title: "Módulo 4",
+    items: ["SubItem 1", "SubItem 2", "SubItem 3"],
+  },
+  {
+    title: "Módulo 5",
+    items: ["SubItem A", "SubItem B", "SubItem C"],
+  },
+];
+
+const Sidebar = () => {
+  return (
+    <>
+      <MobileSidebar modules={modules} />
+      <DesktopSidebar modules={modules} />
+    </>
+  );
+};
+
+export default Sidebar;
