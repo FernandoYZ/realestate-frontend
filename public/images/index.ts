97c5f5412b3ade9d8c674505b05f40f8409a43cb
FernandoYZ <fernandozavala266@gmail.com>
2024-09-09 16:48:28 -0500

    feat(icons): Crear barrel file para exportar íconos SVG como componentes
    - Se creó el archivo index.ts dentro de la carpeta images para exportar los íconos SVG como componentes de React.
    - Los íconos exportados son:
      - AjustesIcon desde ./icons/Ajustes.svg
      - FacturacionIcon desde ./icons/Facturacion.svg
      - LogisticaIcon desde ./icons/Logistica.svg
      - ReporteIcon desde ./icons/Reporte.svg
      - SeguridadIcon desde ./icons/Seguridad.svg
      - ServicioIcon desde ./icons/Servicio.svg
      - TesoreriaIcon desde ./icons/Tesoreria.svg
    
    No hay issues
    #Close 0

---
+++ public/images/index.ts
@@ -0,0 +1,7 @@
+export { default as AjustesIcon } from "./icons/Ajustes.svg";
+export { default as FacturacionIcon } from "./icons/Facturacion.svg";
+export { default as LogisticaIcon } from "./icons/Logistica.svg";
+export { default as ReporteIcon } from "./icons/Reporte.svg";
+export { default as SeguridadIcon } from "./icons/Seguridad.svg";
+export { default as ServicioIcon } from "./icons/Servicio.svg";
+export { default as TesoreriaIcon } from "./icons/Tesoreria.svg";
