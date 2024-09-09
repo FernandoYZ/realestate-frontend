8f51f25b82ed4e8af6f279b0ebb745d1f4497fb9
FernandoYZ <fernandozavala266@gmail.com>
2024-09-09 17:27:49 -0500

    refactor(components): Mejorar DesktopSidebar para manejar selección de módulos
    - Se refactorizó el componente DesktopSidebar.tsx para utilizar la interfaz Module desde sidebar.interface.ts, mejorando la tipificación.
    - Se añadieron propiedades selectedModule y onItemSelect para gestionar la selección de elementos desde el componente padre.
    - Se implementó la visualización de íconos para cada módulo, mejorando la representación visual en el menú.
    - Se actualizó el diseño del encabezado y se mejoró la experiencia de usuario con un estilo más moderno y limpio.
    
    No hay issues
    #Close 0

--- src/components/sections/layouts/Desktop/DesktopSidebar.tsx
+++ src/components/sections/layouts/Desktop/DesktopSidebar.tsx
@@ -2,17 +2,15 @@
 
 import { useState } from "react";
 import MenuDesplegable from "../components/MenuDesplegable";
-
-interface Module {
-  title: string;
-  items: string[];
-}
+import { Module } from "@/core/interfaces/sidebar.interface";
 
 interface DesktopSidebarProps {
   modules: Module[];
+  selectedModule: string | null;
+  onItemSelect: (item: string) => void;
 }
 
-const DesktopSidebar = ({ modules }: DesktopSidebarProps) => {
+const DesktopSidebar = ({ modules, selectedModule, onItemSelect }: DesktopSidebarProps) => {
   const [openMenuIndex, setOpenMenuIndex] = useState<number | null>(null);
 
   const handleToggleMenu = (index: number) => {
@@ -20,25 +18,31 @@ const DesktopSidebar = ({ modules }: DesktopSidebarProps) => {
   };
 
   return (
-    <div className="hidden lg:flex lg:flex-col lg:h-screen lg:w-64 lg:bg-gray-900 lg:text-white lg:p-4 lg:overflow-y-auto lg:fixed">
-      <h1 className="text-2xl font-semibold mb-8">Sidebar</h1>
+    <aside className="hidden lg:flex lg:flex-col lg:h-screen lg:w-64 lg:bg-white lg:text-black lg:p-4 lg:overflow-y-auto lg:fixed shadow-md">
+      <div className="text-xl text-center font-semibold mb-8 uppercase">Inmobiliaria</div>
 
-      {modules.map((module, index) => (
-        <MenuDesplegable
-          key={index}
-          title={module.title}
-          items={module.items}
-          isOpen={openMenuIndex === index}
-          onToggle={() => handleToggleMenu(index)}
-        />
-      ))}
+      {modules.map((module, index) => {
+        const IconComponent = module.icon;
+        return (
+          <MenuDesplegable
+            key={index}
+            title={module.title}
+            items={module.items}
+            isOpen={openMenuIndex === index}
+            onToggle={() => handleToggleMenu(index)}
+            selectedModule={selectedModule}
+            onItemSelect={onItemSelect}
+            icon={IconComponent ? <IconComponent /> : null} 
+          />
+        );
+      })}
 
-      <div className="mt-auto">
+      <div className="mt-auto bg-red-50">
         <button className="w-full bg-blue-600 hover:bg-blue-700 text-white py-2 rounded-md">
           Upgrade Account
         </button>
       </div>
-    </div>
+    </aside>
   );
 };
 
